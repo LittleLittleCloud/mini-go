2aac3cd0e1119880d0542b3bbf41aa4a29e5e1fd
Wai Lun Tam <twailun@gmail.com>
2016-10-14 09:33:40 -0700

    Accept .go file as argument

--- calc.ml
+++ calc.ml
@@ -2,7 +2,13 @@
 open Ast
 open Pp
 
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
+let print_channel ch =
+  let lexbuf = Lexing.from_channel ch in
       let result = Parser.parse Lexer.token lexbuf in
         Printf.printf "%s" (pp_prog result); print_newline(); flush stdout
+
+let _ =
+  if (Array.length Sys.argv) == 1 then
+    print_channel stdin
+  else
+    print_channel (open_in Sys.argv.(1))
