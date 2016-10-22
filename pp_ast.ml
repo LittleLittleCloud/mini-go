474c492123f9580a99e4f1e03fdfc05487c24939
linux <2260578356@qq.com>
2016-10-22 01:26:17 -0700

    allow the void type of func, finish all the typechecking,slightly change the prettyprint (cuz i make a small change on AST. use the type check like this: checkProg [] ast

--- pp_ast.ml
+++ pp_ast.ml
@@ -5,28 +5,31 @@ let rec pp_prog s = match s with
   | Prog (x, y)       -> String.concat "" ["Prog(["; (pp_proc_list x); "],"; (pp_stmt y); ")"]
 
 and pp_proc s = match s with
-  | Proc (x, y, z, w) -> String.concat "" ["Proc("; x; ",["; (pp_param_list y); "],"; (pp_types_option z); ","; (pp_stmt w); ")"]
+  | Proc (x, y, z, w) -> String.concat "" ["Proc(\""; x;"\""; ",["; (pp_param_list y); "],"; "Some(";(pp_types_option z);")"; ","; (pp_stmt w); ")"]
 
-and pp_proc_list s = match s with
+and pp_proc_list s = match s withl
   | []                -> ""
   | hd::[]            -> pp_proc hd
-  | hd::tl            -> pp_proc hd ^ " " ^ pp_proc_list tl
+  | hd::tl            -> pp_proc hd ^ ";" ^ pp_proc_list tl
 
 and pp_param_list s = match s with
   | []                -> ""
-  | (ex,ty)::[]       -> pp_exp ex ^ " " ^ pp_types ty
-  | (ex,ty)::tl       -> pp_exp ex ^ " " ^ pp_types ty ^ ", " ^ pp_param_list tl
+  | (ex,ty)::[]       -> "("^pp_exp ex ^ "," ^ pp_types ty^")"
+  | (ex,ty)::tl       -> "("^pp_exp ex ^ "," ^ pp_types ty ^")"^ "; " ^ pp_param_list tl
 
 and pp_types s = match s with
-  | TyInt             -> "Int"
-  | TyBool            -> "Bool"
-  | TyChan (x)        -> "Chan " ^ (pp_types x)
-  | TyFunc (x, y)     -> "Func [" ^ (pp_types_list x) ^ "] " ^ (pp_types y)
-
+  | TyInt             -> "TyInt"
+  | TyBool            -> "TyBool"
+  | TyChan (x)        -> "TyChan " ^ (pp_types x)
+  | TyFunc (x, y)     ->  begin
+                          match y with
+                          | Some t -> "TyFunc [" ^ (pp_types_list x) ^ "] " ^ (pp_types t)
+                          | None ->"Func (" ^ (pp_types_list x) ^ ") " ^ "None"
+                          end
 and pp_types_list s = match s with
   | []                -> ""
   | hd::[]            -> pp_types hd
-  | hd::tl            -> pp_types hd ^ ", " ^ pp_types_list tl
+  | hd::tl            -> pp_types hd ^ "; " ^ pp_types_list tl
 
 and pp_types_option s = match s with
   | Some(x)           -> pp_types x
@@ -37,7 +40,7 @@ and pp_stmt s = match s with
   | Go (x)            -> String.concat "" ["Go("; pp_stmt x; ")"]
   | Transmit (x, y)   -> String.concat "" ["Transmit("; x; ","; pp_exp y; ")"]
   | RcvStmt (x)       -> String.concat "" ["RcvStmt("; x; ")"]
-  | Decl (x, y)       -> String.concat "" ["Decl("; x; ","; pp_exp y; ")"]
+  | Decl (x, y)       -> String.concat "" ["Decl(\""; x; "\","; pp_exp y; ")"]
   | DeclChan (x)      -> String.concat "" ["DeclChan("; x; ")"]
   | Assign (x, y)     -> String.concat "" ["Assign("; x; ","; pp_exp y; ")"]
   | While (x, y)      -> String.concat "" ["While("; pp_exp x; ","; pp_stmt y; ")"]
@@ -58,8 +61,8 @@ and pp_exp s = match s with
   | RcvExp (x)        -> String.concat "" ["RcvExp("; x; ")"]
   | IConst (x)        -> String.concat "" ["IConst("; string_of_int x; ")"]
   | BConst (x)        -> String.concat "" ["BConst("; string_of_bool x; ")"]
-  | Var (x)           -> String.concat "" ["Var("; x; ")"]
-  | FuncExp(x, y)     -> String.concat "" ["FuncExp("; x; ",["; (pp_exp_list y); "])"]
+  | Var (x)           -> String.concat "" ["Var(";"\""; x;"\""; ")"]
+  | FuncExp(x, y)     -> String.concat "" ["FuncExp(\""; x; "\",["; (pp_exp_list y); "])"]
 
 and pp_exp_list s = match s with
   | []                -> ""
