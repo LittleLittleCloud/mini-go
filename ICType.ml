5d0bff02b9bede98d258fb8b670d413ea5434670
linux <2260578356@qq.com>
2016-10-26 10:09:36 -0700

    Add ICType

---
+++ ICType.ml
@@ -0,0 +1,39 @@
+(* Abstract Syntax Tree (AST) representation of Mini-Go *)
+
+type prog = Prog of (proc list) * stmt
+
+and proc = Proc of string * ((exp * types) list) * (types option) * stmt
+
+and types = TyInt
+           | TyBool
+           | TyChan of types
+           | TyFunc of (types list * (types option))
+                                                                       
+and stmt = Seq of stmt * stmt
+          | Go of stmt
+          | Transmit of string * exp
+          | RcvStmt of string 
+          | Decl of string * exp
+          | DeclChan of string
+          | Assign of string * exp
+          | While of exp * stmt
+          | ITE of exp * stmt * stmt
+          | Return of exp
+          | FuncCall of string * (exp list)
+          | Print of exp
+          | Skip
+          
+and exp = And of exp * exp
+         | Eq of exp * exp
+         | Gt of exp * exp
+         | Plus of exp * exp
+         | Minus of exp * exp
+         | Times of exp * exp
+         | Division of exp * exp
+         | Not of exp
+         | RcvExp of string
+         | IConst of int
+         | BConst of bool
+         | Var of string
+         | FuncExp of string * (exp list)
+         
