f8414f96287d4b4d45e1768b5a7e181c27844273
linux <2260578356@qq.com>
2016-11-13 08:15:32 -0800

    add type checking into the pp.ml

--- pp.ml
+++ pp.ml
@@ -7,6 +7,7 @@ open ICG
 open Pp_print_ICG
 open Pp_fmt
 open FormatAST
+open Typing
 open RenameAST
 open VMType
 open VMC
@@ -15,20 +16,58 @@ open VM
 let pretty_print ch =
   let lexbuf = Lexing.from_channel ch in
       let result = Parser.parse Lexer.token lexbuf in
+      let t=Typing.checkProg [] result in
+      match t with
+      | Some _   -> let fmtAST=FormatAST.normalizeProg result in 
+                    let renameAST=RenameAST.renameProg [] fmtAST in
+                    let i=RenameAST.getNameSupply() in 
+                    ICG.chgLabelSupply i;
+                    let icg=ICG.translateProg [] renameAST in 
+                    let vmc=VMC.genVMC icg in 
+                      Printf.printf "AST:\n%s\n\n%s\nPretty Print:\n%s\nICG:\n%s\nVM:\n%s\n"
+                      (Pp_ast.pp_prog result) 
+                      (Pp_fmt.pp_prog fmtAST)
+                      (Pp_fmt.pp_prog renameAST)
+                      (Pp_print_ICG.pp_irc icg)
+                      (Pp_print_VMC.pp_vmc vmc);
+                      VM.run vmc;
+                      flush stdout
+
+      | None      -> print_endline "error";
+                      VM.run [Halt];
+                      flush stdout
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* 
       let fmtAST=FormatAST.normalizeProg result in 
       let renameAST=RenameAST.renameProg [] fmtAST in
       let i=RenameAST.getNameSupply() in 
       ICG.chgLabelSupply i;
       let icg=ICG.translateProg [] renameAST in 
       let vmc=VMC.genVMC icg in 
+      if t!=None then
+      begin
         Printf.printf "AST:\n%s\n\n%s\nPretty Print:\n%s\nICG:\n%s\nVM:\n%s\n"
         (Pp_ast.pp_prog result) 
         (Pp_fmt.pp_prog fmtAST)
         (Pp_fmt.pp_prog renameAST)
         (Pp_print_ICG.pp_irc icg)
         (Pp_print_VMC.pp_vmc vmc);
-        VM.run vmc;
-        flush stdout
+        VM.run vmc
+      end
+      flush stdout *)
 
 let ch = match (Array.length Sys.argv) with
   | 1 -> stdin
